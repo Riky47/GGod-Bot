6ea2cbb999e2531fca7713902063af45640dfdf0
Riky47 <56732510+Riky47@users.noreply.github.com>
2020-02-11 20:25:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,11 +23,15 @@ client.on('message', msg => {
 
     if (msg.content.startsWith(">")){
         if (msg.content.toLowerCase() === '>cmds') {
-            msg.channel.send(`${msg.member}, Here the commands list: \n` + "``` **v1.0** \n >Cmds     -- commands list\n >Ann/..   -- new normal announcement\n >HAnn/..   -- new highlighted announcement\n >Invite   -- invete link```");
+            msg.channel.send(`${msg.member}, Here the commands list: \n` + "``` **v1.0** \n >Cmds     -- commands list\n >BotsCmds  -- Show all bots commands\n >Ann/..   -- new normal announcement\n >HAnn/..   -- new highlighted announcement\n >Invite   -- invete link```");
         }
         else if (msg.content.toLowerCase().startsWith(">ann/")) {
             if (msg.member.hasPermission('ADMINISTRATOR')) { 
                 var msgArr2 = msg.content.split('/')
+                var Numbers2
+                for (let i = 0; i < msgArr2.length; i++) {
+                    Numbers2 = Numbers2 + msgArr2[i];     
+                }
                 msg.channel.send(`${msgArr2[1]}` + "\n" + `@everyone`);
                 const attachment2 = new Attachment('https://i.imgur.com/Is3xXHQ.gif');
                 msg.channel.send(attachment2);                
@@ -41,7 +45,11 @@ client.on('message', msg => {
         else if (msg.content.toLowerCase().startsWith(">hann/")) {
             if (msg.member.hasPermission('ADMINISTRATOR')) { 
                 var msgArr2 = msg.content.split('/')
-                msg.channel.send("```" + `${msgArr2[1]}` + "```\n" + `@everyone`);
+                var Numbers2
+                for (let i = 0; i < msgArr2.length; i++) {
+                    Numbers2 = Numbers2 + msgArr2[i];     
+                }
+                msg.channel.send("```" + `${msgArr2[Numbers2]}` + "```\n" + `@everyone`);
                 const attachment2 = new Attachment('https://i.imgur.com/Is3xXHQ.gif');
                 msg.channel.send(attachment2);                
                 msg.delete();
